9747679c8707741ab58c61e6c1cafd29daa3afb4
mskrzypiecWsb <chx100232@student.chorzow.wsb.pl>
2023-06-25 01:34:11 +0200

    lab07

---
+++ 1. formatowanie czasu/Program.cs
@@ -0,0 +1,24 @@
+﻿DateTime aktualnyCzas = DateTime.Now;
+int godziny = aktualnyCzas.Hour;
+int minuty = aktualnyCzas.Minute;
+int sekundy = aktualnyCzas.Second;
+
+int czasWSekundach = liczCzasWSekundach(godziny, minuty, sekundy);
+string czas = liczCzas(czasWSekundach);
+Console.WriteLine(czas);
+
+    static int liczCzasWSekundach(int godziny, int minuty, int sekundy)
+{
+    int czasWSekundach = (godziny * 3600) + (minuty * 60) + sekundy;
+    return czasWSekundach;
+}
+
+static string liczCzas(int czasWSekundach)
+{
+    int godziny = czasWSekundach / 3600;
+    int minuty = (czasWSekundach % 3600) / 60;
+    int sekundy = czasWSekundach % 60;
+
+    string czas = godziny.ToString("D2") + ":" + minuty.ToString("D2") + ":" + sekundy.ToString("D2");
+    return czas;
+}
---
+++ 2. dlugosc odcinka w ukladzie kartezjanskim dwuwymiarowym/Program.cs
@@ -0,0 +1,15 @@
+﻿double x1 = 2.5;
+double y1 = 3.8;
+double x2 = 5.1;
+double y2 = 7.2;
+
+double dlugoscOdcinka = liczDlugosc(x1, y1, x2, y2);
+Console.WriteLine("Długość odcinka: " + dlugoscOdcinka);
+
+    static double liczDlugosc(double x1, double y1, double x2, double y2)
+{
+    double deltaX = x2 - x1;
+    double deltaY = y2 - y1;
+    double dlugoscOdcinka = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+    return dlugoscOdcinka;
+}
